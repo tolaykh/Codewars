17d89cf375ec82ff2ee075db79cfc258ca1e15de
tolaykh <Tolaykh@outlook.com>
2025-07-24 20:26:10 +0300

    Update CreditCardFile.c

--- C/7kyu/CreditCardFile.c
+++ C/7kyu/CreditCardFile.c
@@ -8,18 +8,18 @@ char *maskify (char *masked, const char *string)
   i = 0;
   c = 0;
     while(string[i] != '\0')
-      {
+    {
       len++;
       i++;
-      }
+    }
     while(c < len)
-      {
+    {
       if(c < len - 4)
         masked[c] = '#';
       else
         masked[c] = string[c];
     c++;
-      }
+    }
     masked[len] = '\0';
   return masked;
 }
